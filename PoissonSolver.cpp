7a5acf263799a9c574b9582640e0d8ef63f1e69d
Michael Bauer <michael.bauer.tsv@gmail.com>
2013-11-07 21:08:16 +0100

    Update my LaTex files.

--- PoissonSolver.cpp
+++ PoissonSolver.cpp
@@ -1,14 +1,17 @@
 #include "classes.h"
 
+void writeToFile(LowerPoissonMatrix&,UpperPoissonMatrix&);
+void readFromFile(LowerPoissonMatrix&,UpperPoissonMatrix&);
+
 int main(int argc, char const *argv[]) {
-	int arg, n;//, dim;
-	if (argc > 1) {
+    int arg, n;//, dim;
+    if (argc > 1) {
         arg = atoi(&*argv[1]);
     } else {
         arg = 4;
     }
     n=arg-1;
-	//dim = n*n;
+    //dim = n*n;
 
     PoissonMatrix A(n);
     Operators O(n);
@@ -19,20 +22,51 @@ int main(int argc, char const *argv[]) {
     UpperPoissonMatrix U(n);
 
     //Run.CG(A,O,V.x,V.b);
-    Run.modifiedIncompleteLU(A,L,U);
+    //Run.modifiedIncompleteLU(A,L,U);
     //Run.incompleteLU(A,L,U);
-    printf("done\n");
+    //printf("done\n");
+    //writeToFile(L,U);
+    readFromFile(L,U);
     // A.printMat();
     //L.printMat();
     //U.printMat();
 
     Run.PCG(A,O,L,U,V.x,V.b);
     
-    for(int i=0;i<n*n;i++)
-        printf("%f ", V.x[i]);
-    printf("\n");
+    // for(int i=0;i<dim;i++)
+    //     printf("%f ", V.x[i]);
+    // printf("\n");
 
-    scanf("%d",&n);
+    return EXIT_SUCCESS;
+}
 
-	return EXIT_SUCCESS;
+void writeToFile(LowerPoissonMatrix& L, UpperPoissonMatrix& U) {
+    FILE *file;
+    file=fopen("LU.txt", "w");
+    if(file==NULL)
+        printf("ERROR!\n");
+    for(int i=0;i<L.size();i++) {
+        for(int j=0;j<L.size();j++) {
+            fprintf(file, "%f\n", L.get(i,j));
+            fprintf(file, "%f\n", U.get(i,j));
+        }
+    }
+    fclose (file);
+}
+
+void readFromFile(LowerPoissonMatrix& L,UpperPoissonMatrix& U) {
+    FILE *file;
+    file = fopen("LU.txt", "r");
+    double l,u;
+    if(file==NULL)
+        printf("ERROR!\n");
+    for(int i=0;i<L.size();i++) {
+        for(int j=0;j<L.size();j++) {
+            fscanf(file, "%lf\n", &l);
+            fscanf(file, "%lf\n", &u);
+            L.set(i,j,l);
+            U.set(i,j,u);
+        }
+    }
+    fclose(file);
 }
